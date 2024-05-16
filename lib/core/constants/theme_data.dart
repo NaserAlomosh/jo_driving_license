c6eb3b193fa9171c71d833e0af4f68ad37e57cda
Tasneem <t.zaareer@futeric.com>
2024-05-16 10:01:51 +0300

    edit theme

--- lib/core/constants/theme_data.dart
+++ lib/core/constants/theme_data.dart
@@ -1,40 +1,24 @@
 import 'package:flutter/material.dart';
 
-//!  colorBorder   =   outLine
-//!  colorPrimary   =   primary
-//!  colorOnPrimary   =   oPrimary
-//!  colorFont   =    secondery
-//!  colorFont2   =   onSecondery
-//!  colorGreen   =   primaryContainer
-//!  colorRed   =     secondaryContainer
-//!  colorGrey   =    tertiaryContainer
-//
+//!  background   =    background
+//!  most used   =   primary
+//!  font   =    secondery
+//!  border   =   outLine
+
 // theme light mode
 ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
   colorScheme: const ColorScheme.light(
-    // colorPrimary
-    // primary: Color.fromARGB(255, 22, 67, 82),
-    primary: Color(0xff7C73E6),
-    // colorOnPrimary
-    onPrimary: Color(0xFFFFFFFF),
-    // colorButtons and Icons
-    secondary: Color(0xffF58025),
-    // color Font
-    onSecondary: Color.fromARGB(91, 255, 255, 255),
-    // background
     background: Color.fromARGB(255, 255, 255, 255),
-    // background: Color.fromARGB(171, 3, 0, 54),
-    // colorGreen
+    onBackground: Color.fromARGB(255, 230, 229, 245),
+    primary: Color(0xff7C73E6),
+    secondary: Color.fromARGB(255, 77, 74, 110),
+    onPrimary: Color(0xFFFFFFFF),
+    onSecondary: Color.fromARGB(91, 255, 255, 255),
     primaryContainer: Color(0xff01DC82),
-    // colorBorder
     outline: Color(0xffEBECF1),
-    // colorRed
     secondaryContainer: Color(0xfff43f5e),
-    // colorGrey
-    // tertiaryContainer:const Color(0xffEBECF1),
     tertiaryContainer: Color(0xFFDDDDDD),
-    // black color
     tertiary: Colors.black,
   ),
 );

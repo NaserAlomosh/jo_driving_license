c5371ad86e38520947ffefa85a3d9bf2f3857ccc
NaserAlomosh <naseralomosh33@gmail.com>
2024-05-18 18:59:48 +0300

    Fix Code

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -8,6 +8,7 @@ import 'package:jo_driving_license/features/home/view_model/cubit.dart';
 import 'package:jo_driving_license/features/questions/view/quistions_view.dart';
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import 'package:jo_driving_license/features/score/exam_score_view.dart';
 import 'package:jo_driving_license/features/score/level_score_view.dart';
 <<<<<<< HEAD
@@ -21,6 +22,10 @@ import 'package:jo_driving_license/features/score/score_view.dart';
 =======
 
 >>>>>>> 3f53868 (Fix)
+=======
+import 'package:jo_driving_license/features/score/exam_score_view.dart';
+import 'package:jo_driving_license/features/score/level_score_view.dart';
+>>>>>>> 42e7299 (Fix Code)
 import '../../../core/constants/image_path.dart';
 import '../../../core/helper/spacing.dart';
 import '../../../core/widgets/error_widget/error_widget.dart';
@@ -40,6 +45,7 @@ class HomeView extends StatelessWidget {
           children: [
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 44ae3d1 (Fix Code)
             IconButton(
@@ -50,6 +56,8 @@ class HomeView extends StatelessWidget {
 <<<<<<< HEAD
                 icon: const Icon(Icons.star)),
 =======
+=======
+>>>>>>> 42e7299 (Fix Code)
             Row(
               children: [
                 IconButton(
@@ -64,6 +72,7 @@ class HomeView extends StatelessWidget {
                     icon: const Icon(Icons.star)),
               ],
             ),
+<<<<<<< HEAD
 >>>>>>> 4abcb0a (add LevelScoreView)
 =======
                 icon: Icon(Icons.star)),
@@ -73,6 +82,8 @@ class HomeView extends StatelessWidget {
                   Icons.star,
                 )),
 >>>>>>> 3f53868 (Fix)
+=======
+>>>>>>> 42e7299 (Fix Code)
             _getCard(context),
             heightSpace(20),
             _getListCategory(),
